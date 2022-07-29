8b6c30fa3b5c3b5dca865e5d6df5f3c1a26c327c
Asep Herman Nursalam <asephnursalam@gmail.com>
2022-07-29 14:20:17 +0700

    initial commit2

--- android/src/main/java/io/github/asephermann/plugins/autostartmanager/AutoStartManagerPlugin.kt
+++ android/src/main/java/io/github/asephermann/plugins/autostartmanager/AutoStartManagerPlugin.kt
@@ -1,15 +1,19 @@
 package io.github.asephermann.plugins.autostartmanager
 
 import com.getcapacitor.JSObject
+import com.getcapacitor.Plugin
+import com.getcapacitor.PluginCall
+import com.getcapacitor.PluginMethod
+import com.getcapacitor.annotation.CapacitorPlugin
 
 @CapacitorPlugin(name = "AutoStartManager")
 class AutoStartManagerPlugin : Plugin() {
     private val implementation = AutoStartManager()
     @PluginMethod
     fun echo(call: PluginCall) {
-        val value: String = call.getString("value")
+        val value = call.getString("value")
         val ret = JSObject()
-        ret.put("value", implementation.echo(value))
+        ret.put("value", value?.let { implementation.echo(it) })
         call.resolve(ret)
     }
 }
